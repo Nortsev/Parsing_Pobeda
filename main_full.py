c668c3c6d98420878a9410d98b57696fcd96390c
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2021-04-20 20:31:43 +0400

    Add create photo album vk

--- main_full.py
+++ main_full.py
@@ -6,11 +6,14 @@ import base_sqllite as sql
 import TEST_IMAGE_BASE as save_img
 import vk_api_pobeda as vk_api
 
+ALBUM_NAME = 'Repost'
 link = [
 
     "https://xn--c1aesfx9dc.xn---63-5cdesg4ei.xn--p1ai/catalog/telefony/sotovye-telefony/?k=false&q=20&s=high&c=57&cg=143&a=0&f[]=400&",
     "https://xn--c1aesfx9dc.xn---63-5cdesg4ei.xn--p1ai/catalog/kompyuternaya-tehnika/?k=false&q=20&s=high&c=57&cg=99&a=0&f[]=400&",
-    "https://xn--c1aesfx9dc.xn---63-5cdesg4ei.xn--p1ai/catalog/instrument/?k=false&q=20&s=high&c=57&cg=84&a=0&f[]=400&"
+    "https://xn--c1aesfx9dc.xn---63-5cdesg4ei.xn--p1ai/catalog/instrument/?k=false&q=20&s=high&c=57&cg=84&a=0&f[]=400&",
+    "https://энгельс.победа-63.рф/catalog/avto/?k=false&q=20&s=high&c=57&cg=132&a=0&f[]=400&",
+
 ]
 HEADERS = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 '
                          '(KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
@@ -68,11 +71,9 @@ def main():
     photos = [product['image'] for product in products]
     captions = [f"{product['title']} \n  Цена: {product['price']} \n Ссылка на товар на нашем сайте: {product['url']}"
                 for product in products]
-    # for product in products:
-    #     photos.append(product['image'])
-    #     captions.append(f"{product['title']} \n  Цена: {product['price']}")
-
-    vk_api.post_group_wall(photos, captions, album_id=279184675)
+    create_session = vk_api.create_session()
+    album_id = vk_api.get_album_id(create_session, ALBUM_NAME)
+    vk_api.post_group_wall(photos, captions, album_id=album_id)
 
 
 if __name__ == "__main__":
--- vk_api_pobeda.py
+++ vk_api_pobeda.py
@@ -4,11 +4,11 @@ from typing import List
 import config
 
 
-def create_session(login, password):
+def create_session(login=config.login, password=config.password):
     vk_session = vk_api.VkApi(login=login, password=password)
     try:
         vk_session.auth(token_only=True)
-        print("ok")
+        print("Соеденение с вк установленно")
     except vk_api.AuthError as error_connect:
         print(error_connect)
     return vk_session
@@ -28,6 +28,7 @@ def post_wall(vk_session, owner_id: int, text_message: str, vk_photos: List[str]
         'message': text_message,
         'attachments': vk_photos_id  # 'photo615022059_457240069,photo615022059_457240054'
     })
+    print("Пост в вк готов")
     return vk_post
 
 
@@ -39,3 +40,13 @@ def post_group_wall(photos_path: list, captions: list, album_id: str):
     return vk_post
 
 
+def get_album_id(vk_session, ALBUM_NAME):
+    photo_albums = vk_session.method('photos.getAlbums', {"owner_id": 615022059})
+    vk_albums = photo_albums['items']
+    for album in vk_albums:
+        if album['title'] == ALBUM_NAME:
+            print('Такой альбом уже имется')
+            return album['id']
+    album = vk_session.method('photos.createAlbum', {"title": ALBUM_NAME})
+    print('Создал новый альбом')
+    return album["id"]
